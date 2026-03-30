17e90abb7af1c478514f8ad05d2265648d3d5d9b
Daniel <dani4lpha@gmail.com>
2026-03-30 23:36:59 +0200

    updated version

--- Kriechrennen/Tausendfuessler.cs
+++ Kriechrennen/Tausendfuessler.cs
@@ -1,6 +1,6 @@
 namespace Kriechrennen;
 
-// Eigenes Kriechtier: bewegt sich schnell in kurzen Schüben (1-3), aber manchmal sehr weit (Zufall < 10%)
+// taussendfuessler schnell in kurzen Schüben (1-3), aber manchmal sehr weit (Zufall < 10%)
 class Tausendfuessler : Kriechtier
 {
     public Tausendfuessler(Random rand) : base(rand)
@@ -8,7 +8,7 @@ class Tausendfuessler : Kriechtier
         symbol = ">>>>>";
     }
 
-    // Normalerweise 1-3 Schritte, selten (10%) ein großer Sprung von 8
+    // 1-3 Schritte, mit 10% großer Sprung (8)
     public override void Krieche()
     {
         if (rand.Next(0, 10) == 0)
